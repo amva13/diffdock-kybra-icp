c1fc9fd627e8fa04f75bd7dc160554ac54eb8008
Alejandro Velez <alex@alexv24.com>
2024-02-11 16:19:24 -0700

    completed HTTP call to diffdock flask server

--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
-from kybra import query, update, void
+from kybra import (
+    Async,
+    CallResult,
+    ic,
+    match,
+    query, 
+    update, 
+    void
+)
+
+from kybra.canisters.management import (
+    HttpResponse,
+    HttpTransformArgs,
+    management_canister,
+)
 
 # This is a global variable that is stored on the heap
 protein: str = ''
@@ -42,3 +56,29 @@ def run_sim() -> void:
 def run_diffdock() -> void:
     # TODO: run diffdock on set inputs
     pass
+
+@update
+def run_diffdock_demo() -> Async[HttpResponse]:
+    global sim_ran
+    http_result: CallResult[HttpResponse] = yield management_canister.http_request(
+        {
+            "url": "https://diffdock-flask.azurewebsites.net/icp/demo",
+            "max_response_bytes": 2_000,
+            "method": {"get": None},
+            "headers": [],
+            "body": None,
+            "transform": {"function": (ic.id(), "xkcd_transform"), "context": bytes()},
+        }
+    ).with_cycles(50_000_000)
+
+    sim_ran = True
+
+    return match(http_result, {"Ok": lambda ok: ok, "Err": lambda err: ic.trap(err)})
+
+@query
+def xkcd_transform(args: HttpTransformArgs) -> HttpResponse:
+    http_response = args["response"]
+
+    http_response["headers"] = []
+
+    return http_response
