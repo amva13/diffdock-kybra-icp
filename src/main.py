6c70660aa2d11134ad1b0d8ed96eb730f221ed17
Alejandro Velez <alex@alexv24.com>
2024-02-11 15:53:48 -0700

    implemented API for diffdock canister

--- src/main.py
+++ src/main.py
@@ -1,16 +1,44 @@
 from kybra import query, update, void
 
 # This is a global variable that is stored on the heap
-message: str = ''
+protein: str = ''
+ligand: str = ''
+result: str = 'tmp'
+sim_ran: bool = False
 
 # Query calls complete quickly because they do not go through consensus
 @query
-def get_message() -> str:
-    return message
+def get_results() -> str:
+    return protein+","+ligand+","+result
+    # if sim_ran:
+    #     return {
+    #         "protein":protein, 
+    #         "ligand":ligand, 
+    #         "result":result,
+    #         "error": "False",
+    #         "message":""
+    #         }
+    # else:
+    #     return {
+    #         "error": "True",
+    #         "message": "simulation has not been run"
+    #     }
 
 # Update calls take a few seconds to complete
 # This is because they persist state changes and go through consensus
 @update
-def set_message(new_message: str) -> void:
-    global message
-    message = new_message # This change will be persisted
+def set_message(protein_smiles: str, ligand_smiles: str) -> void:
+    global protein
+    global ligand
+    protein = protein_smiles 
+    ligand = ligand_smiles
+
+@update
+def run_sim() -> void:
+    global sim_ran
+    run_diffdock()
+    sim_ran = True
+
+def run_diffdock() -> void:
+    # TODO: run diffdock on set inputs
+    pass
